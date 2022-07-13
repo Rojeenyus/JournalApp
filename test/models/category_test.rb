0abb6e7de0523b507c9d32feeace6f0b48fa8fdf
Rojeenyus <@rojeeneckashbondoc@gmail.com>
2022-07-13 20:27:45 +0800

    added delete edit

---
+++ test/models/category_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+def test_creating
+  @category = Category.new
+
+  assert_equal @category.save, true
+end
+
+def test_task_association
+  @category = Category.create(name: "Personal")
+
+  assert_equal @category.tasks.length, 0
+end
+end
